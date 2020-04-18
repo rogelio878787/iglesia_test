ba537335d552e22c7517cfa95eab0b1c9048e310
Rogelio <rogeliogc345@gmail.com>
2020-04-18 17:12:47 -0500

    Updates

---
+++ precache-manifest.338763f18d65af3de98b1ebfb0e498cc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15cddaf494fdcf3206cb784eb22119c6",
+    "url": "/iglesia_test/index.html"
+  },
+  {
+    "revision": "3b3ee43d4eaa18c5b443",
+    "url": "/iglesia_test/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "b8b5684c20a2df261d6e",
+    "url": "/iglesia_test/static/css/main.70cc85b7.chunk.css"
+  },
+  {
+    "revision": "3b3ee43d4eaa18c5b443",
+    "url": "/iglesia_test/static/js/2.8d8985e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iglesia_test/static/js/2.8d8985e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8b5684c20a2df261d6e",
+    "url": "/iglesia_test/static/js/main.7ba87bff.chunk.js"
+  },
+  {
+    "revision": "889c9bb40d554c778cf8",
+    "url": "/iglesia_test/static/js/runtime-main.a8432c17.js"
+  },
+  {
+    "revision": "186916ad2feeea11d97816c0c8273092",
+    "url": "/iglesia_test/static/media/iglesia.186916ad.jpeg"
+  },
+  {
+    "revision": "719fa26a3cfb867884dd08320a6d4c97",
+    "url": "/iglesia_test/static/media/pastores.719fa26a.jpg"
+  }
+]);
